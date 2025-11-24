b99872be9b3e36e78ea7db6f39a5ff3bba4a0624
adhnanjeff <adhnanjeff26@gmail.com>
2025-11-24 16:53:35 +0530

    fix: updated debater

--- 3_crew/debate/src/debate/main.py
+++ 3_crew/debate/src/debate/main.py
@@ -18,7 +18,7 @@ def run():
     Run the crew.
     """
     inputs = {
-        'motion': 'There needs to be strict laws to regulate LLMs',
+        'motion': 'Periods in woman are overrated',
     }
     
     try:
